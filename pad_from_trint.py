f4728703337fd8ece6f51b602cd8150de620a1a4
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2018-12-27 17:56:24 +0100

    Read trint transcripts in SRT instead of SBV

--- pad_from_trint.py
+++ pad_from_trint.py
@@ -3,22 +3,27 @@
 
 import sys
 import re
+from sbv_from_srt import sbv_from_srt
+
 
 def chunks(file):
     """Read `file`, splitting it at doubled linebreaks"""
     lines = []
     for line in file:
         lines.append(re.sub(' {2,}', ' ', line.strip()))
+
     return '\n'.join(lines).split('\n\n')
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        sys.exit("usage: ./{} transcript.sbv".format(sys.argv[0]))
+        sys.exit("usage: ./{} transcript.srt".format(sys.argv[0]))
 
     transcript = []
     try:
         with open(sys.argv[1], 'r') as file:
-            transcript = chunks(file)
+            raw = file.read().split('\n')
+            transcript = chunks(sbv_from_srt(raw).split('\n'))
     except IOError as err:
         sys.exit("Transcript not readable: {}").format(err)
 
@@ -27,7 +32,7 @@ if __name__ == '__main__':
     for chunk in transcript:
         lines = chunk.split('\n')
 
-        for line in lines[2:]:
+        for line in lines[1:]:
             if line == '':
                 continue
             out += ' ' + line
