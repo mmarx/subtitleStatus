66311357f5497d04d779d6b3fe911fadf3de6dbc
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2018-12-28 16:41:30 +0100

    Update requirements

--- init_test_database.sh
+++ init_test_database.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 python manage.py migrate
-python manage.py set_default_site --name localhost --domain localhost:8000
 python manage.py createsuperuser --username admin --email root@localhost
 python manage.py loaddata 32c3
 python import_languages.py
